1c819291efbb6e3493f83d82b92049e64298115d
DavidRprt <davirapo@gmail.com>
2024-06-08 15:56:27 -0300

    Adding visa section

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 import Activities from "@/components/Activities";
 import DestinationsCarousel from "@/components/DestinationsCarousel";
+import EmailSection from "@/components/EmailSection";
+import VisaHelp from "@/components/VisaHelp";
 
 export default function Home() {
   return (
     <>
       <DestinationsCarousel />
       <Activities />
+      <VisaHelp />
+      <EmailSection />
     </>
   );
 }
---
+++ components/EmailSection.tsx
@@ -0,0 +1,16 @@
+"use client"
+import React, { useState } from "react"
+import Link from "next/link"
+import Image from "next/image"
+
+const EmailSection = () => {
+
+  return (
+    <section
+    >
+
+    </section>
+  )
+}
+
+export default EmailSection
